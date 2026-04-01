819e8eaac2d3327e90862372155d8c987796d47e
Francesco Cassese <francescocassese9@gmail.com>
2026-04-01 14:31:16 +0200

    Soluzione 01_saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
@@ -5,9 +7,19 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+// --- Funzione classica ---
+
+function salutaUtente(nome) {
+    return `Ciao ${nome}`;
+}
+
+// --- Arrow Function --- NOME DIVERSO PER LASCIARLI ENTRAMBI "ATTIVI" EVITANDO CHE IN CONSOLE ESCA L'ERRORE!
+
+const benvenutoUtente = nome => `Ciao ${nome}`
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(salutaUtente(userName));
+console.log(benvenutoUtente(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
