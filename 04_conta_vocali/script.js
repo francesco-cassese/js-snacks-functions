c35de66b2c43879e76f66b78035c4be0a70ec472
Francesco Cassese <francescocassese9@gmail.com>
2026-04-01 17:02:04 +0200

    Aggiunto array per poter mostrare anche le vocali nell'esercizio 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -6,23 +6,27 @@ const word = 'javascript';
 // Dichiara la funzione qui.
 
 function contatoreVocali(parola) {
-    let contatore = 0                                     // dichiaro variabile contatore e la setto a 0
+    let contatore = 0
+    const vocaliTrovate = [];                                     // dichiaro variabile contatore e la setto a 0 
     const listaVocali = ["a", "e", "i", "o", "u"];        // creo una lista contenente le vocali
     for (let i = 0; i < parola.length; i++) {             // inizio il primo ciclo sulla parola passata
         const letteraCorrente = parola[i].toLowerCase();  // assegno la variabile ad ogni interazione e mi assicuro che la parola passata si minuscola 
         for (let j = 0; j < listaVocali.length; j++) {    // contemporaneamente inizio un secondo ciclo sulla lista delle vocali 
             if (letteraCorrente === listaVocali[j]) {     // se la lettera corrente è uguale ad una delle vocali
-                contatore++                               // aumento il contatore
+                contatore++
+                vocaliTrovate.push(listaVocali[j]);                              // aumento il contatore
             }
         }
     }
-    return contatore                                      // mi restituisce il contatore 
+    return [contatore, vocaliTrovate];                                      // mi restituisce il contatore 
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-const vocaliTrovate = console.log(contatoreVocali(word));
+const vocaliTrovate = contatoreVocali(word);
+
+console.log(`Le vocali trovate sono ${vocaliTrovate[0]} e sono (${vocaliTrovate[1]})`);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
