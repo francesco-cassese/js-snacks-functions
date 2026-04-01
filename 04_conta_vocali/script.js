aea2ad06dd7b5ab68da53a3158142023f6aa89bf
Francesco Cassese <francescocassese9@gmail.com>
2026-04-01 15:32:29 +0200

    Soluzione 04_conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,24 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function contatoreVocali(parola) {
+    let contatore = 0
+    const listaVocali = ["a", "e", "i", "o", "u"];
+    for (let i = 0; i < parola.length; i++) {
+        const letteraCorrente = parola[i].toLowerCase();
+        for (let j = 0; j < listaVocali.length; j++) {
+            if (letteraCorrente === listaVocali[j]) {
+                contatore++
+            }
+        }
+    }
+    return contatore
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const vocaliTrovate = console.log(contatoreVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
