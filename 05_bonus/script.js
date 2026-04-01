f91b5dc5cfbf097c5b33de8f75a2788039bba53e
Francesco Cassese <francescocassese9@gmail.com>
2026-04-01 16:16:16 +0200

    Soluzione 05_bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,29 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function getStatoGiornata(oraAttuale) {
+
+    if (oraAttuale < 13) {         // Se oraAttuale è minore delle 13 restituisci -1 
+        return -1
+    }
+    if (oraAttuale <= 17) {        // Se oraAttuale è minore delle 17 restituisci 0
+        return 0
+    }
+    if (oraAttuale > 17) {         // Se oraAttuale è maggiore delle 17 restituisci 1
+        return 1
+    }
+}
+
+let buongiornoUtente = getStatoGiornata(18);
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+if (buongiornoUtente === -1) {
+    console.log(`Buongiorno ${name}`);
+} else if (buongiornoUtente === 0) {
+    console.log(`Buonpomeriggio ${name}`);
+} else if (buongiornoUtente === 1) {
+    console.log(`Buonasera ${name}`);
+}
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
