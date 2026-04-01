307de318aec876f031738f80efdfdfdfcd524b01
Francesco Cassese <francescocassese9@gmail.com>
2026-04-01 15:15:21 +0200

    Soluzione 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function filtraNomi(nomi, lettera) {
+    let nomiFiltrati = [];                   // Creo lista vuota
+    for (let i = 0; i < nomi.length; i++) {  // Inizio il ciclo 
+        let nomeCorrente = nomi[i];          // inizializzo e assegno la variabile ad ogni interazione 
+        if (nomeCorrente[0] === lettera) {   // Se la prima lettera del nome è uguale alla lettera che gli abbiamo passato
+            nomiFiltrati.push(nomeCorrente); // Push del nome corrente in nomi filtrati
+        }
+    }
+    return nomiFiltrati                      // Restituisce l'array nomi filtrati 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const listaNomiFiltrati = console.log(filtraNomi(names, `A`));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
